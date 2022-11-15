aceb195dab3597ffc190fde9effb2d711628663e
Shivanand <Shivanand0297@gmail.com>
2022-11-16 00:04:52 +0530

    comments added

--- LCO/05 todo (reducers)/todo-app/src/context/reducer.js
+++ LCO/05 todo (reducers)/todo-app/src/context/reducer.js
@@ -1,8 +1,9 @@
 import {ADD_TODO, REMOVE_TODO} from './action.types'
 
-export default (state, action)=>{   //this is todoReducer
+export default (state, action)=>{   //this is todoReducer will return new state
     switch (action.type) {  // it is an object which consists of variety of things
         case ADD_TODO:
+            console.log([...state, action.payload]);    //[{id:, todoString: }]
             return [...state, action.payload]   //payload is the information it carries along with it, in our case it is our todo string and we want to add it to the state
     
         case REMOVE_TODO:
