3020db689d571e7cfb15ba067ec18386a767a59b
Shivanand <Shivanand0297@gmail.com>
2022-11-21 15:00:38 +0530

    defined main functions

--- LCO/06 useeffect and local storage/useeffect-with-local-storage/src/App.js
+++ LCO/06 useeffect and local storage/useeffect-with-local-storage/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Container} from "reactstrap"
+import { useState, useEffect } from 'react'
+import Todoform from './components/Todoform'
+import Todos from './components/Todos'
+import  "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 
-function App() {
+
+const App = () => {
+  const [todos, setTodos] = useState([])
+
+  // to load all the todos stored in the local storage
+  useEffect(()=>{
+    const localTodos = localStorage.getItem("todos")
+    console.log({localStorage});
+    if(localTodos){
+      setTodos(JSON.parse(localTodos))
+    }
+  }, [])
+
+  // added into state
+  const addTodos = async (todo) =>{
+    setTodos([...todos, todo])
+  }
+
+  // adding into local storage
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);  // whenever there is change in the todos, run this method and add that into local storage
+
+  const removeTodo = (id) =>{
+    setTodos(todos.filter(todo=>todo.id !== id))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid>
+      <h1>Todo with local storage</h1>
+      <Todos todos={todos} removeTodo={removeTodo}/>
+      <Todoform addTodos={addTodos}/>
+    </Container>  
+  )
 }
 
-export default App;
+export default App
