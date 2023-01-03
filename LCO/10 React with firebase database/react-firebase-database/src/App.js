3f9406445c3f871b2f3887b876d3e80e16f89926
Shivanand <Shivanand0297@gmail.com>
2023-01-03 14:25:38 +0530

    getting all the data from the database and added pagenotfound and viewcontacts

--- LCO/10 React with firebase database/react-firebase-database/src/App.js
+++ LCO/10 React with firebase database/react-firebase-database/src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useEffect } from "react";
 
-function App() {
+import { Container, Col, Row } from "reactstrap";
+
+// react-router-dom3
+import { BrowserRouter as Router, Switch, Route, Link, Routes } from "react-router-dom";
+
+// react toastify stuffs
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+// firebase stuffs
+//import firebase config and firebase database
+import {firebaseConfig} from "./utils/Config"
+import firebase from "firebase/app";
+import "firebase/database"
+import "firebase/storage"
+
+// components
+import AddContact from "./pages/AddContact";
+import Contacts from "./components/Contact";
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import ViewContact from "./pages/ViewContact";
+import PageNotFound from "./pages/PageNotFound";
+
+// context api stuffs
+// import reducers and contexts
+import reducer from "./context/reducer"
+import { ContactContext } from "./context/Context";
+import {SET_CONTACT, SET_LOADING} from "./context/action"
+
+//initlizeing firebase app with the firebase config which are in ./utils/firebaseConfig
+//initialize FIREBASE
+firebase.initializeApp(firebaseConfig)
+
+// first state to provide in react reducer
+const initialState = {
+  contacts: [],
+  contact: {},
+  contactToUpdate: null,
+  contactToUpdateKey: null,
+  isLoading: false
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // will get contacts from firebase and set it on state contacts array
+  const getContacts = async () => {
+    //load existing data, loading icon
+    dispatch({
+      type: SET_LOADING,
+      payload: true
+    })
+
+    const contactsRef = await firebase.database().ref("/contacts")
+    contactsRef.on("value", snapshot=>{ //value will look for all the values in the database 
+                                        //and spanshot to get all the data in snapshot
+      dispatch({
+        type: SET_CONTACT,
+        payload: snapshot.val()
+      })
+      dispatch({
+        type: SET_LOADING,
+        payload: false
+      })
+
+
+    }) 
+
+
+
+  };
+  
+
+  // getting contact  when component did mount
+  useEffect(() => {
+    getContacts()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ContactContext.Provider value={{state, dispatch}} >
+        <ToastContainer />
+        <Header />
+        <Container>
+          <Routes>
+            <Route path="/contact/add" element={AddContact} />
+            <Route path="/contact/view" element={ViewContact} />
+            <Route path="/" element={Contacts} />
+            <Route path="*" element={PageNotFound} />
+          </Routes>
+        </Container>
+
+        <Footer />
+      </ContactContext.Provider>
+    </Router>
   );
-}
+};
 
 export default App;
--- LCO/10 React with firebase database/react-firebase-database/src/utils/Config.js
+++ LCO/10 React with firebase database/react-firebase-database/src/utils/Config.js
@@ -1,4 +1,12 @@
-
+export const firebaseConfig = {
+    apiKey: "AIzaSyAWDrYylHK68n2Ub3BbaKbMV8XUW0d2j6M",
+    authDomain: "gitapp-firebase.firebaseapp.com",
+    projectId: "gitapp-firebase",
+    storageBucket: "gitapp-firebase.appspot.com",
+    messagingSenderId: "1056661561409",
+    appId: "1:1056661561409:web:ddc18040642f06571666c0",
+    measurementId: "G-GJY1EJMMRB"
+  };
 
 // image config from browser-image-resizer
 export const imageConfig = {
