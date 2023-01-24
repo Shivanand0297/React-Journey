ed706498f929e3aa0aef13c34a17254212a5df32
Shivanand <Shivanand0297@gmail.com>
2023-01-24 12:20:15 +0530

    created reducer function

---
+++ Code With Harry/03 redux/redux/src/state/reducers/reducer.js
@@ -0,0 +1,12 @@
+export default reducer = (state = 0, action) =>{
+    switch(action.type){
+        case "deposit":
+            return state + action.payload
+        case "withdraw":
+            return state - action.payload
+        default:
+            return state
+    }
+}
+
+// 2️⃣
