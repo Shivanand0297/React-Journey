9698a5d5a5dc051e21a963c79be021f6d6b52c3c
Shivanand <Shivanand0297@gmail.com>
2023-01-24 12:19:29 +0530

    added reducers

---
+++ Code With Harry/03 redux/redux/src/state/reducers/index.js
@@ -0,0 +1,9 @@
+// combining all the reducers
+import { combineReducers } from "redux";
+import reducer from "./reducer";
+
+export default reducers = combineReducers({
+    reducer: reducer
+})
+
+// 3️⃣
