eb66491a10c649bc9344e5576929343a56015ee2
shivanand <shivanand02071997@gmail.com>
2023-06-29 11:09:10 +0530

    query client provider

--- 16-react-query/src/index.js
+++ 16-react-query/src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { QueryClientProvider, QueryClient} from "react-query"
-import { ReactQueryDevtools } from "react-query/devtools"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { client } from "./queryClientProvider";
 
-const client = new QueryClient()
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <QueryClientProvider client={client}>
     <App />
-    <ReactQueryDevtools/>
+    <ReactQueryDevtools />
   </QueryClientProvider>
 );
