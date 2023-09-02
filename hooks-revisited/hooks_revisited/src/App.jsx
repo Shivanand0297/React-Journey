e8b588a6b76b7a36386b923bd5a7d27daa29e6ec
shivanand <shivanand02071997@gmail.com>
2023-09-03 00:48:51 +0530

    functional initialization

--- hooks-revisited/hooks_revisited/src/App.jsx
+++ hooks-revisited/hooks_revisited/src/App.jsx
@@ -1,35 +1,27 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FunctionalInitialization/>
     </>
   )
 }
 
 export default App
+
+const FunctionalInitialization = () => {
+  const [state, setState] = useState(() => console.log("hello"))
+
+  // ! State will reinitialize again and again on every rerender
+  // to avoid this use lazy initialization using function which return some value
+
+  return (
+    <>
+      <button
+        onClick={() => setState(Math.random())}
+      > State: {state}</button>
+    </>
+  )
+}
